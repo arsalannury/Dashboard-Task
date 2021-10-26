d5176fa3b238e8817b8c5db6f29571ab13e684a1
arsalannury <arsiiiland@yahoo.com>
2021-10-26 14:08:08 +0330

    add click to minor buttons

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,6 +2,9 @@ const userIcon = document.querySelector('.user-icon');
 const itemsNavbar = document.querySelector('.items-navbar');
 const sidebarMenu = document.querySelector('.items-child-sidebar');
 const managerItem = document.querySelector('.manager-item');
+const signOut = document.querySelector('.sign-out');
+const addUser = document.getElementById('adduser');
+const usersList = document.getElementById('userlist');
 
 // :: :: :: :: :: :: :: ::
 //  navbar menu JS
@@ -32,8 +35,25 @@ managerItem.addEventListener('click',(e) => {
     
 })
 
+// :: :: :: :: :: :: :: ::
+//  Sign Out button
+// :: :: :: :: :: :: :: ::
+signOut.addEventListener('click',(e) => {
+    setTimeout(() => {location.replace(' ./Login.html') },1000)
+})
 
+// :: :: :: :: :: :: :: ::
+//  go to add user page
+// :: :: :: :: :: :: :: ::
+addUser.addEventListener('click',(e) => {
+    setTimeout(() => {location.href = './AddUser.html' },1000)
+})
 
-
+// :: :: :: :: :: :: :: ::
+//  go to users page
+// :: :: :: :: :: :: :: ::
+usersList.addEventListener('click' , (e) => {
+    setTimeout(() => {location.href = './Users.html' },1000)
+})
 
 
