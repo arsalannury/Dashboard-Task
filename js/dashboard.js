3845dbeb554244c6abdd3015e832678e4384a4b2
alirezaJonaqi <alirezaJonaqinet@gmail.com>
2021-10-28 18:53:49 +0330

    change password modal complete

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,7 +4,7 @@ const sidebarMenu = document.querySelector('.items-child-sidebar');
 const managerItem = document.querySelector('.manager-item');
 const signOut = document.querySelector('.sign-out');
 const addUser = document.getElementById('adduser');
-const usersList = document.getElementById('userlist');
+const usersListBtn = document.getElementById('userlist');
 const userName = document.querySelector('.user-name');
 
 // :: :: :: :: :: :: :: ::
@@ -57,7 +57,7 @@ addUser.addEventListener('click',(e) => {
 // :: :: :: :: :: :: :: ::
 //  go to users page
 // :: :: :: :: :: :: :: ::
-usersList.addEventListener('click' , (e) => {
+usersListBtn.addEventListener('click' , (e) => {
     setTimeout(() => {location.href = './Users.html' },1000)
 })
 
@@ -79,21 +79,44 @@ changePassBtn.addEventListener('click', () => {
     modalEl.classList.add('modalActive');
 });
 
+// :: :: :: :: :: :: :: ::
 // cancel change password modal
+// :: :: :: :: :: :: :: ::
 const cancelChangePass = document.querySelector('#modal_changePasswordCancel');
 cancelChangePass.addEventListener('click', () => {
     modalEl.classList.remove('modalActive');
 }); 
 
+
+// :: :: :: :: :: :: :: ::
 // confirm change password modal
+// :: :: :: :: :: :: :: ::
 const confirmChangePass = document.querySelector('#modal_changePasswordConfirm');
 const newPass = document.querySelector('#newPass');
 const repeatPass = document.querySelector('#repeatPass');
+const logedInUserName = document.querySelector('.user-name').textContent.trim();
 
 confirmChangePass.addEventListener('click', () => {
     if (newPass.value === repeatPass.value) {
 
+        let userIndex = usersList.findIndex(item => item.username.toLocaleLowerCase() === logedInUserName.toLocaleLowerCase());
+        if (userIndex >= 0) {
+            //update pass
+            usersList[userIndex].password = newPass.value;
+            setUsers(usersList);
+            
+            //alert txt
+            document.querySelector('.modal_alertTxt').textContent = 'password Changed!';
+            document.querySelector('.modal_alertTxt').classList.add('modal_alertSuccess');
+            
+            //go to login page
+            setTimeout(() => {
+                window.location.href = 'login.html';
+            }, 1000);
+
+        }
+
     }else {
-        document.querySelector('.modal_alertTxt').classList.add('modal_alertTxtActive')
+        document.querySelector('.modal_alertTxt').classList.add('modal_alertTxtActive');
     }
 }); 
