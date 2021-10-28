ffb3435286148f3d80a06655fa20cdef1f6b60b5
Agha alikhani <alikhani.dev@gmail.com>
2021-10-28 15:19:21 +0330

    upload image - get image in localstorage

--- assets/js/uploadImage.js
+++ assets/js/uploadImage.js
@@ -1,7 +1,7 @@
-//! variable
-const users = getLsUsers()
-
 //! Event listener
+document.addEventListener('DOMContentLoaded', () => {
+  appendImages(getImages())
+})
 document.querySelector('#add').addEventListener('click', () => {
   setUsers(users)
   window.location.href = 'http://localhost:3000/Users.html'
@@ -10,6 +10,11 @@ document.querySelectorAll('input[type="file"]').forEach((inp, index) => {
   showImage(inp, inp.nextElementSibling, index)
 })
 
+//! variable
+const users = getLsUsers()
+const idUser = window.location.search.slice(4)
+const user = users.find(u => u.id === Number(idUser))
+
 //! function
 
 //? Check valid image and set image for display
@@ -17,23 +22,42 @@ function showImage (src, target, index) {
   var fr = new FileReader()
   // when image is loaded, set the src of the image where you want to display it
   fr.onload = () => {
-    const idUser = window.location.search.slice(4)
     target.src = fr.result
-    setImg(idUser, fr.result, index)
+    setImg(fr.result, index)
   }
   src.addEventListener('change', () => {
+    const label = src.previousElementSibling
     // fill fr with image data
     if (src.files[0].type.startsWith('image')) {
       fr.readAsDataURL(src.files[0])
-      src.previousElementSibling.hidden = true
+      label.hidden = true
     } else {
-      src.previousElementSibling.hidden = false
+      label.hidden = false
     }
   })
 }
 
 //? set image in localStorage
-function setImg (id, img, index) {
-  const user = users.find(u => u.id == id)
+function setImg (img, index) {
   user.images[`file${index}`] = img
+}
+//? get image in localStorage
+function getImages () {
+  let allImages = {}
+  for (const key in user.images) {
+    const img = new Image()
+    img.src = user.images[key]
+    allImages[key] = img
+  }
+  return allImages
+}
+//? append images in body
+function appendImages (images) {
+  for (const key in images) {
+    // get index input file
+    const indexItem = key.substr(-1)
+    const img = document.querySelector(`#formFile_${indexItem}`).nextElementSibling
+    document.querySelector(`#formFile_${indexItem}`).previousElementSibling.hidden = true
+    img.parentNode.replaceChild(images[key], img)
+  }
 }
